b0256eda79af9c996abe8df6e719007e4d215a3e
CisThard <he3717@gmail.com>
2025-02-16 12:54:48 +0900

    style: 색상명 대문자에서 소문자로 변경 chore: 테스트용 import 추가

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,6 +6,11 @@ import CheckModal from "./modals/CheckModal";
 import Button from "../components/Button/Button";
 import LoginCheckModal from "./modals/LoginCheckModal";
 import PopUpModal from "./modals/PopUpModal";
+import Container from "@/components/Container/Container";
+import TextBox from "@/components/TextBox/TextBox";
+import Reply from "@/components/Reply/Reply";
+import Search from "@/components/Search/Search";
+import Card from "@/components/Card/Card";
 
 export default function Home() {
   // 삭제제 사유 모달 창 State 변수
