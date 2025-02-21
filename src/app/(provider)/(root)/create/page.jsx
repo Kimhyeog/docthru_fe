297a38501b4917dc3bebaacbfe3a0fd2b2449ec1
sungmin <usm0259@kaist.ac.kr>
2025-02-21 17:00:58 +0900

    style: 신규 챌린지 신청 배경색 변경

--- src/app/(provider)/(root)/create/page.jsx
+++ src/app/(provider)/(root)/create/page.jsx
@@ -1,8 +1,4 @@
 "use client";
-import Image from "next/image";
-import logo from "@/assets/logo.svg";
-import style from "./Create.module.css";
-import Link from "next/link";
 import CreateChallengeForm from "./_components/CreateChallengeForm";
 
 function CreateChallengePage() {
