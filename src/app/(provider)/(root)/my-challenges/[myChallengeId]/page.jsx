44dbd0b9e45d2d75032831ff6c1d42fe4b50f0f6
19013219_kim <cyworld5057@naver.com>
2025-02-26 18:23:46 +0900

    feat:MyChallengeIdPage 디자인, css ui 구현 완료 //이미지 링크 빼고

--- src/app/(provider)/(root)/my-challenges/[myChallengeId]/page.jsx
+++ src/app/(provider)/(root)/my-challenges/[myChallengeId]/page.jsx
@@ -1,4 +1,3 @@
-import ChipCardStatus from "@/components/Chips/ChipCardStatus";
 import styles from "./DeletedOrRejected.module.css";
 import clockIcon from "@/assets/ic_clock.svg";
 import Image from "next/image";
@@ -7,6 +6,9 @@ import Chip from "@/components/Chips/Chip";
 import ChipCategory from "@/components/Chips/ChipCategory";
 import dayjs from "dayjs";
 import peopleIcon from "@/assets/ic_people.svg";
+import Link from "next/link";
+import nextImg from "@/assets/images/nextJs.png";
+import { FaArrowRight } from "react-icons/fa6";
 
 async function DeletedOrRejectedPage({ params }) {
   const param = await params;
@@ -23,8 +25,6 @@ async function DeletedOrRejectedPage({ params }) {
   const challengeTitle = challenge.title;
   const challengeContent = challenge.content;
   const deadline = challenge.deadline;
-
-  console.log(challenge);
 
   const statusTitle = [
     { value: "DELETED", title: "삭제된 챌린지입니다.", subTitle: "삭제사유" },
@@ -45,46 +45,64 @@ async function DeletedOrRejectedPage({ params }) {
     return "알 수 없는 상태입니다.";
   };
 
+  const docUrl = challenge.docUrl;
+
   return (
-    <div className={styles.Container}>
-      <div className={styles.header}>
-        <div className={styles.statusTitle}>{getTitle(status)}</div>
-        <div className={styles.statusCommentContainer}>
-          <p className={styles.commentTitle}>{getSubTitle(status)}</p>
-          <div>{reasonComment}</div>
-        </div>
-      </div>
-      <div className={styles.cardContainer}>
-        <div className={styles.challengeTitle}>{challengeTitle} </div>
-        {/* chips */}
-        <div className={styles.chips}>
-          <Chip type={type}>{type}</Chip>
-          <ChipCategory category={challenge.docType} />
-        </div>
-        <div className={styles.challengeContent}>{challengeContent}</div>
-        <div className={styles.footer}>
-          <div className={styles.date}>
-            <Image src={clockIcon} alt="마감 시계" width={24} height={24} />
-            <span>
-              {deadline
-                ? dayjs(deadline).format("YYYY년 MM월 DD일 마감")
-                : "2024년 3월 3일 마감"}
-            </span>
-          </div>
-          <div className={styles.participants}>
-            <Image src={peopleIcon} alt="참여 인원" width={24} height={24} />
-            <span>
-              {participants && maxParticipants
-                ? ` ${participants}/${maxParticipants} 참여중`
-                : "15/15 참여 완료"}
-            </span>
+    <>
+      <div className={styles.Container}>
+        <div className={styles.header}>
+          <div className={styles.statusTitle}>{getTitle(status)}</div>
+          <div className={styles.statusCommentContainer}>
+            <p className={styles.commentTitle}>{getSubTitle(status)}</p>
+            <div>{reasonComment}</div>
           </div>
         </div>
+        <div className={styles.cardContainer}>
+          <div className={styles.challengeTitle}>{challengeTitle}</div>
+          <div className={styles.chips}>
+            <Chip type={type}>{type}</Chip>
+            <ChipCategory category={challenge.docType} />
+          </div>
+          <div className={styles.challengeContent}>{challengeContent}</div>
+          <div className={styles.footer}>
+            <div className={styles.date}>
+              <Image src={clockIcon} alt="마감 시계" width={24} height={24} />
+              <span>
+                {deadline
+                  ? dayjs(deadline).format("YYYY년 MM월 DD일 마감")
+                  : "2024년 3월 3일 마감"}
+              </span>
+            </div>
+            <div className={styles.participants}>
+              <Image src={peopleIcon} alt="참여 인원" width={24} height={24} />
+              <span>
+                {participants && maxParticipants
+                  ? ` ${participants}/${maxParticipants} 참여중`
+                  : "15/15 참여 완료"}
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className={styles.contentContainer}>
+          <p className={styles.linkTitle}>원문 링크</p>
+          <div className={styles.imgContainer}>
+            <Image
+              className={styles.urlImg}
+              src={nextImg}
+              alt="Next.js 로고"
+              objectFit="cover"
+            />
+            <Link
+              target="_blank"
+              href={challenge.docUrl}
+              className={styles.button}
+            >
+              링크 열기 {<FaArrowRight />}
+            </Link>
+          </div>
+        </div>
       </div>
-      <div className={styles.contentContainer}>
-        <p className={styles.linkTitle}>원문 링크</p>
-      </div>
-    </div>
+    </>
   );
 }
 
