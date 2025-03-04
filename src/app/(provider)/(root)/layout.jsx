6eebe97b8bf86d033db0fb824d6ee954d5731efc
gitit-emkoo <kwcc2020@naver.com>
2025-03-04 13:54:36 +0900

    style:푸터 여백 조정 후 전체적인 배경색 요구사항대로 수정

--- src/app/(provider)/(root)/layout.jsx
+++ src/app/(provider)/(root)/layout.jsx
@@ -10,9 +10,19 @@ function RootLayout({ children }) {
   const isHeaderHidden =
     (pathname.includes("/works/") && pathname.endsWith("/edit")) ||
     (pathname.includes("/challenges/") && pathname.endsWith("/challengeTask"));
+
+  // 패스네임 힌트로 저도 페이지별 배경색 다르게 했다요
+  const backgroundColor =
+    pathname === "/challenges" || pathname === "/my-challenges"
+      ? "#FAFAFA"
+      : "white";
+
   return (
     <div
-      style={{ fontFamily: basicFont.style.fontFamily }}
+      style={{
+        fontFamily: basicFont.style.fontFamily,
+        backgroundColor: backgroundColor,
+      }}
       className={style.layout}
     >
       {!isHeaderHidden && <Header />}
